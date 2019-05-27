30e63ee9a70c90ae4b601db6265d3dc3a1f69fa0
steffen alvarez <steffen.alvarez@etu.univ-amu.fr>
2019-05-27 09:29:00 +0200

    EXO 3 step 1

--- src/main/java/fr/univ_amu/iut/exercice3/Joueur.java
+++ src/main/java/fr/univ_amu/iut/exercice3/Joueur.java
@@ -1,6 +1,7 @@
 package fr.univ_amu.iut.exercice3;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.image.Image;
 
 public class Joueur {
@@ -12,42 +13,56 @@ public class Joueur {
     private IntegerProperty score;
 
     Joueur(String fileName) {
-        throw new RuntimeException("Not yet implemented !");
+        image = new Image(getClass().getClassLoader().getResource(fileName).toExternalForm());
+        score = new SimpleIntegerProperty(0);
     }
 
     public static void initialiserScores() {
-        throw new RuntimeException("Not yet implemented !");
+        BLANC.initialiserScore();
+        NOIR.initialiserScore();
+        PERSONNE.initialiserScore();
     }
 
     private void initialiserScore() {
-        throw new RuntimeException("Not yet implemented !");
+        setScore(0);
     }
     
     public IntegerProperty scoreProperty() {
-        throw new RuntimeException("Not yet implemented !");
+        return score;
     }
 
     public int getScore() {
-        throw new RuntimeException("Not yet implemented !");
+        return score.get();
+
     }
     
     private void setScore(int score) {
-        throw new RuntimeException("Not yet implemented !");
+        this.score.set(score);
+
     }
 
     public void decrementerScore() {
-        throw new RuntimeException("Not yet implemented !");
+        score.set(score.get() - 1);
+
     }
 
     public void incrementerScore() {
-        throw new RuntimeException("Not yet implemented !");
+        score.set(score.get() + 1);
+
     }
 
     public Image getImage() {
-        throw new RuntimeException("Not yet implemented !");
+        return image;
+
     }
 
     public Joueur suivant() {
-        throw new RuntimeException("Not yet implemented !");
+        if (this == NOIR ){
+            return BLANC;
+        }
+        else if (this == BLANC){
+            return  NOIR;
+        }
+        else return PERSONNE;
     }
 }
